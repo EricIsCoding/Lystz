1455020873c2a4d51d80b05f05744415a91b8f40
EricJWilhite <ericjwilhite@gmail.com>
2021-01-29 12:16:27 -0800

    removing extras

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,6 +1,8 @@
 class Vendor < ApplicationRecord
     validates :name, presence: true
 
+    belongs_to :user
+
     has_many :blocks, :dependent => :destroy
     has_many :items, through: :blocks, :dependent => :destroy
 end
