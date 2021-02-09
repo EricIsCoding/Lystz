463e224c564453e9262d842a9e8b579a599465b4
EricJWilhite <ericjwilhite@gmail.com>
2021-02-08 18:14:19 -0800

    Add vendor_name to serializer for ease of use

--- app/serializers/block_serializer.rb
+++ app/serializers/block_serializer.rb
@@ -2,6 +2,10 @@ class BlockSerializer
   include JSONAPI::Serializer
   attributes :name, :creator
 
+  attribute :vendorName do |object|
+    "#{object.vendor.name}"
+  end
+
   has_many :items
   belongs_to :vendor
 end
