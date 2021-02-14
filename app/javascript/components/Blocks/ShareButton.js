98b6b2a57a1391a7d21f84c5b9451c43cf98aa8d
EricJWilhite <ericjwilhite@gmail.com>
2021-02-14 10:59:12 -0800

    Add ShareButton

--- app/javascript/components/Blocks/ShareButton.js
+++ app/javascript/components/Blocks/ShareButton.js
@@ -1,14 +1,27 @@
 import React, { useState } from 'react'
+import {useDispatch} from 'react-redux'
 import ToggleButton from 'react-bootstrap/ToggleButton'
 import { updateBlock } from '../../actions/blockActions'
 
 const ShareButton = (props) => {
-    const [checked, setChecked] = useState(false);
+    const [checked, setChecked] = useState(props.share);
+    const dispatch = useDispatch();
+
+    const updateShare = (event) => {
+        let block = {
+            id: props.id,
+            data: {
+                share: event.currentTarget.checked
+            }
+        }
+        setChecked(event.currentTarget.checked)
+        dispatch(updateBlock(block))
+    }
 
     return(
         <>
          <ToggleButton
-            key={props.BlockId}
+            key={props.id}
             type="checkbox"
             className="w-20 m-2"
             size="sm"
@@ -17,7 +30,7 @@ const ShareButton = (props) => {
             value="1"
             onChange={(e) => updateShare(e)}
         > 
-        Share? 
+        {checked ? "Shared!" : "Share?"}
         </ToggleButton>
         </>
     )
