e9cfb762f5c3c6b3c1968f4b5c2b3d0f167002f2
EricJWilhite <ericjwilhite@gmail.com>
2021-02-17 14:04:52 -0800

    Add actions to userReducer

--- app/javascript/reducers/userReducer.js
+++ app/javascript/reducers/userReducer.js
@@ -5,10 +5,26 @@ export const userReducer = produce((user, action) => {
     switch (action.type) {
         case "API_FETCH_SUCCESS":
           return action.data.user
+        case "ADD_GROUP_SUCCESS":
+            user.group = {...action.payload.attributes.group}      
+            return user
+        case "ACCEPT_GROUP_INVITE":
+            user.group = {...action.payload.attributes.group}
+            return user
+        case "DECLINE_GROUP_INVITE":
+            user.group = {}
+            return user
+        case "INCREASE_SHARED_COUNT":
+            user.group.sharedBlockCount += 1
+            return user
+        case "DECREASE_SHARED_COUNT":
+            user.group.sharedBlockCount -= 1
+            return user
         case "REMOVE_USER":      
             delete user[action.deleteData.id]
             return user
         case "UPDATE_USER":
-            return state
+            user[action.user.id] = action.user
+            return user
     }
 }, {})
