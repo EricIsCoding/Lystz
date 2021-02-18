c82b53626c98c28518509f91761ac960f44e305b
EricJWilhite <ericjwilhite@gmail.com>
2021-02-17 20:23:37 -0800

    Cleaned-up body of items

--- app/javascript/components/Items/Item.js
+++ app/javascript/components/Items/Item.js
@@ -4,8 +4,19 @@ import Card from 'react-bootstrap/Card'
 import DeleteButton  from '../DeleteButton'
 
 const Item = (props) => {
+     
+    const renderBody = (bodyProps) => {
+        const bodyStrings = []
 
-   
+        for(const key in bodyProps) {
+            if(bodyProps[key] && bodyProps[key] != "null") {
+                bodyStrings.push(`${key}: ${bodyProps[key]}`)
+            }
+        }
+
+        return bodyStrings.join(" | ")
+    }
+
     return(
     <Card>
         <Accordion.Toggle as={Card.Header} eventKey={props.id}>
@@ -13,10 +24,7 @@ const Item = (props) => {
         </Accordion.Toggle>
         <Accordion.Collapse eventKey={props.id}>
         <Card.Body>
-            Brand: {props.brand} | Quantity: {props.quantity}
-            <br />
-            Description: {props.description}
-            <br />
+            <p>{renderBody({'Description': `${props.description}`, 'Quantity': `${props.quantity}`, 'Brand': `${props.brand}`})}</p>
             <DeleteButton type="item" id={props.id} vendorId={props.vendorId} blockId={props.blockId}/>
         </Card.Body>
         </Accordion.Collapse>
